76cb54165dd3ce2555a9ab7d8c792c5f3865b249
Christopher Comella <git.chrisco@gmail.com>
2016-10-13 13:53:29 +0200

    swapNum

---
+++ swapNum.js
@@ -0,0 +1,9 @@
+"use strict"
+
+function swapNum(n1, n2) {
+	console.log(`Before swap: n1: ${n1}, n2: ${n2}`);
+  [n2, n1] = [n1, n2];
+	console.log(`After swap n1: ${n1}, n2: ${n2}`);
+}
+
+console.log(swapNum(1, 2));
