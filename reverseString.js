b1a9ce5274640624aaf0b27721fa4f9f4b786983
Christopher Comella <git.chrisco@gmail.com>
2016-10-20 15:19:01 -0400

    reverseString3

--- reverseString.js
+++ reverseString.js
@@ -3,15 +3,30 @@
 function reverseString1(str) {
 	return str.split('').reverse().join('');
 }
-
 console.log(reverseString1('foo'));
 
 function reverseString2(str) {
 	let reversed = '';
+
 	for (let char of str) {
 		reversed = char + reversed;
 	}
+
 	return reversed;
 }
-
 console.log(reverseString2('bar'));
+
+function reverseString3(str) {
+	let reversed = [];
+
+	if (!str || typeof str != 'string' || str.length < 2) {
+		return str;
+	}
+
+	for (let i = str.length - 1; i >= 0; i--) {
+		reversed.push(str[i]);
+	}
+
+	return reversed.join('');
+}
+console.log(reverseString2('baz'));
